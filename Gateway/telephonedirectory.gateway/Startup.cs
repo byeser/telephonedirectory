843323bf40e5535f6ed9b675d6394476d66af59c
MuhammetESER <esermuhammet@hotmail.com>
2023-01-21 18:16:03 +0300

    projenin oluşturulması , apigateway ve consule implementasyonu

---
+++ Gateway/telephonedirectory.gateway/Startup.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using System.Web;
+
+namespace telephonedirectory.Gateway
+{
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services) {
+            services.Configure<IISServerOptions>(options => {
+                options.AllowSynchronousIO = true;
+            });
+            services.AddCors(c => {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            }); 
+            services.Configure<IISOptions>(options => options.AutomaticAuthentication = true);
+            services.Configure<AuthSettings>(Configuration.GetSection("AuthSettings"));
+            services.AddMvcCore(options => {
+                options.EnableEndpointRouting = false;
+
+            }).SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Latest);
+
+            services.AddHttpContextAccessor();
+
+            services.AddControllers();
+
+            var mvcBuilder = services.AddMvc();
+
+            services.AddOcelot().AddConsul();
+            services.AddControllers();
+            services.AddSwaggerForOcelot(Configuration);
+
+            services.AddCors(o =>
+            {
+                o.AddPolicy("AllowOrigin",
+                    builder =>
+                    {
+                        List<string> corsUrls = new List<string>();
+                        Configuration.GetSection("AppSettings").Bind("Cors", corsUrls);
+                        builder.WithOrigins(corsUrls.ToArray())
+                            .SetIsOriginAllowedToAllowWildcardSubdomains()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials();
+                    });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseSwaggerAuthorized();
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            //app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseCors("AllowOrigin");
+
+
+            var forwardingOptions = new ForwardedHeadersOptions() { ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.All };
+            app.UseForwardedHeaders(forwardingOptions);
+           
+            app.UseAuthentication();
+            app.UseStaticFiles();
+            app.UseAuthorization();
+            app.UseResponseCaching();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", "API v1");
+                c.RoutePrefix = string.Empty;
+            });
+            app.UseCors(x => x
+                 .AllowAnyOrigin()
+                 .AllowAnyMethod()
+                 .AllowAnyHeader());
+            app.Use(async (context, next) => {
+                context.Response.Headers.Add("X-Xss-Protection", "1;mode=block");// 1:Xss active ,Reflected  XSS durumunda sanitize yapmadan direk render işlemini engeller
+                context.Response.Headers.Add("Referrer-Policy", "strict-origin-when-cross-origin");
+                await next();
+            });
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwaggerForOcelotUI(Configuration, opt => { opt.DownstreamSwaggerEndPointBasePath = "/swagger/docs"; }).UseOcelot().Wait();
+        }
+    }
+}
---
+++ Gateway/telephonedirectory.gateway/SwaggerBasicAuthMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net;
+using System.Text;
+
+namespace telephonedirectory.Gateway
+{
+    public class SwaggerBasicAuthMiddleware
+    {
+        private readonly RequestDelegate next;
+        private IOptions<AuthSettings> _options;
+        public SwaggerBasicAuthMiddleware(RequestDelegate next, IOptions<AuthSettings> options)
+        {
+            this.next = next;
+            _options = options;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/swagger"))
+            {
+                string authHeader = context.Request.Headers["Authorization"];
+                if (authHeader != null && authHeader.StartsWith("Basic "))
+                {
+                    var encodedUsernamePassword = authHeader.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries)[1]?.Trim();
+                    var decodedUsernamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
+
+                    var username = decodedUsernamePassword.Split(':', 2)[0];
+                    var password = decodedUsernamePassword.Split(':', 2)[1];
+
+                    if (IsAuthorized(username, password))
+                    {
+                        await next.Invoke(context);
+                        return;
+                    }
+                }
+                context.Response.Headers["WWW-Authenticate"] = "Basic";
+
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;              
+            }
+            else
+            {
+                await next.Invoke(context);
+            }
+
+        }
+        public bool IsAuthorized(string username, string password)
+        {
+            return (username.Equals(_options.Value.Username, StringComparison.InvariantCultureIgnoreCase)
+                    && password.Equals(_options.Value.Password));
+
+        }
+    }
+    public static class SwaggerAuthorizeExtensions
+    {
+        public static IApplicationBuilder UseSwaggerAuthorized(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SwaggerBasicAuthMiddleware>();
+        }
+    }
+}
